522b17b2f48f8b2c0f53b04002a3e7a21a5bdbb4
Jewel Malimban <krisdiajewls@gmail.com>
2024-09-15 02:01:32 +0800

    Initial Setup

---
+++ src/main/java/com/project/alims/AlimsApplication.java
@@ -0,0 +1,13 @@
+package com.project.alims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/alims/AlimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.alims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

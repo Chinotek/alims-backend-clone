4f130152d52bd05c7e602a3b286cb02e6a7b97e8
DjinnSuba <oysuba@up.edu.ph>
2024-10-14 10:13:14 +0800

    [Feat] Reagents Model

---
+++ src/main/java/com/project/alims/model/Reagents.java
@@ -0,0 +1,38 @@
+package com.project.alims.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reagents")
+public class Reagents {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "material_id", nullable = false)
+    private Material material;
+
+    @Column(name = "qty", nullable = false)
+    private Integer qty;
+
+    @Column(name = "unit", nullable = false)
+    private Integer unit;
+
+    @Column(name = "location", nullable = false)
+    private String location;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiry_date;
+
+    @Column(name = "cost", nullable = false)
+    private Integer cost;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}

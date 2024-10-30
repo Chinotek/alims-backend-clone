69b435647db4cb97a1ce8e4a79c7f2127a06ec6c
rencionoel <rencionoel@gmail.com>
2024-10-30 23:01:21 +0800

    feat(BorrowForm Crud): basic crud

--- src/main/java/com/project/alims/repository/BorrowFormRepository.java
+++ src/main/java/com/project/alims/repository/BorrowFormRepository.java
@@ -1,4 +1,9 @@
 package com.project.alims.repository;
 
-public class BorrowFormRepository {
+import com.project.alims.model.BorrowForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BorrowFormRepository extends JpaRepository<BorrowForm, Long> {
 }

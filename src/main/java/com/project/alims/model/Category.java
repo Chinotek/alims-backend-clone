b4509b9abbe66edc003e97735f84827ca1a4f57f
Jewel Malimban <krisdiajewls@gmail.com>
2024-11-18 23:14:51 +0800

    feat: category not unique shortname

--- src/main/java/com/project/alims/model/Category.java
+++ src/main/java/com/project/alims/model/Category.java
@@ -11,7 +11,7 @@ public class Category {
     @Column(name = "category_id")
     private Long categoryId;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String shortName;
 
     @Column(name = "subcategory_1", nullable = false)

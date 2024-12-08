b2d66fbec8150fdbc52b6467de27ed06b1ffacd4
rencionoel <rencionoel@gmail.com>
2024-12-08 22:26:48 +0800

    swag fix 4

--- src/main/java/com/project/alims/CorsConfig.java
+++ src/main/java/com/project/alims/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("localhost:8080")
+                .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("https://alims-pgh.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true); // If you need to include cookies in the request
